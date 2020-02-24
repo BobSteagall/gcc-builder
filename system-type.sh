6890d91abd79825bfca7be8a1d7ed9ae21690ef0
Bob Steagall <bob.steagall.cpp@gmail.com>
2020-02-24 11:00:10 -0500

    Improve system type detection; separate script install dir from binary install dir

--- system-type.sh
+++ system-type.sh
@@ -10,10 +10,23 @@ if [ "$platform_os" = "Linux" ]; then
     platform_arch=`uname -p`
     platform_dist=`lsb_release -si`
     platform_vnum=`lsb_release -sr | cut -d '.' -f 1`
+    platform_mvnum=`lsb_release -sr | cut -d '.' -f 2`
 
-    if [ -f /etc/redhat-release ] || [ -f /etc/centos-release ] ; then
+    if [ -f /etc/centos-release ] ; then
 
-        platform_desc='el'
+        platform_desc='centos'
+
+    elif [ -n "`echo $platform_dist | grep -i CentOS`" ] ; then
+
+        platform_desc='centos'
+
+    elif [ -f /etc/redhat-release ] ; then
+
+        platform_desc='rhel'
+
+    elif [ -n "`echo $platform_dist | grep -i RedHatEnterprise`" ] ; then
+
+        platform_desc='rhel'
 
     elif [ "$platform_dist" = "Ubuntu" ]; then
 
@@ -30,6 +43,7 @@ elif [ "$platform_os" = "FreeBSD" ]; then
     platform_dist=$platform_os
     platform_desc='bsd'
     platform_vnum=`uname -r | cut -d '.' -f 1`
+    platform_mvnum=`lsb_release -sr | cut -d '.' -f 2`
 
 else
 
@@ -37,6 +51,12 @@ else
     platform_dist='unknown'
     platform_desc='unknown'
     platform_vnum='0.0'
+
+fi
+
+if [ -n "$platform_mvnum" ]; then
+
+    platform_vnum="$platform_vnum.$platform_mvnum"
 
 fi
 
@@ -51,6 +71,10 @@ elif [ $1 = "--full" ]  ||  [ $1 = "-f" ]; then
          $platform_dist $platform_vnum \
          $platform_arch                \
          $platform_desc$platform_vnum
+
+elif [ $1 = "--tag" ]  ||  [ $1 = "-t" ]; then
+
+    echo $platform_desc$platform_vnum
 
 fi
 
